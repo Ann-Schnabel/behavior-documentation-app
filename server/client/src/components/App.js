11756469fe2fea4df88206ef3b8cb7b0f53ec7df
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-08-12 11:38:50 -0400

    created Home page setup

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -3,12 +3,15 @@ import '../styles/App.css';
 import { Switch, Route } from "react-router"
 
 import Login from './Login'
+import Home from './Home'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path={"/"} component={Login} />
+        <Route path={"/login"} component={Login} />
+        <Route path={"/home"} component={Home} />
       </Switch>
     </div>
   );
