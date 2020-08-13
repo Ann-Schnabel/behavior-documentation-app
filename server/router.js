add1d6fd34a45d6cb53ce8d891249481fe181763
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-08-13 15:52:48 -0400

    added routes for adding a teacher, and creating a student for a teacher

--- server/router.js
+++ server/router.js
@@ -16,4 +16,42 @@ module.exports = function (router) {
       response.send(teacher.students);
     });
   });
+
+  //route to add a teacher to the database
+  // takes in firstName, lastName in request.body
+  router.post("/api/teacher", (request, response, next) => {
+    let newTeacher = new models.Teacher({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+    })
+    newTeacher.save();
+    response.send(newTeacher);
+  })
+
+
+  //route to create students to add to a teacher
+  // takes in teacherId, firstName (student), lastName, dob
+
+  router.put("/api/teacher/students", (request, response, next) => {
+    let newStudent = new models.Student({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      dob: request.body.dob,
+    })
+
+    models.Teacher.findById(request.body.teacherId)
+    .populate("students")
+    .exec((error, teacher) => {
+      if (error) return response.send(error.message);
+
+      teacher.students.push(newStudent);
+      teacher.save();
+      newStudent.save();
+      response.send(teacher.students);
+    })
+
+
+  })
+
+
 }
