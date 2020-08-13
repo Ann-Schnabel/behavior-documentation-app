784e269664cfb338753c3c439e1c7369fa8fdf35
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-08-13 14:28:41 -0400

    NavBar Added

--- server/client/src/components/Home.js
+++ server/client/src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/Home.css';
 
+import NavBar from './NavBar'
 
 class Home extends Component {
   render() {
     return (
         <div>
+            <NavBar />
             <h1>Hello, World</h1>
         </div>
       );
---
+++ server/client/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+
+function NavBar() {
+  return (
+    <div>
+        <div className='nav'>
+            <div id='logo-square'></div>
+            <h1 id='logo'>Logo</h1>
+        </div>
+    </div>
+  );
+}
+
+export default NavBar;
