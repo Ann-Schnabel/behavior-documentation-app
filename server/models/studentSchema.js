6636d76e7648b973b072f7709d81c63369baa252
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-08-13 11:18:56 -0400

    added data models

---
+++ server/models/studentSchema.js
@@ -0,0 +1,125 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/BehaviorTracking');
+
+//variable schema to hold different recording schemas
+const recordingSchema = new Schema({}, {strict: false});
+
+//supported recording types
+const RecordingTypes = ["eventRecording", "durationRecording", "latencyRecording", "intervalRecording"];
+
+const behaviorSchema = new Schema({
+  behaviorName: String,
+  recordingType: String,
+  data: [recordingSchema]
+})
+
+
+const Behavior = mongoose.model("behaviorModel", behaviorSchema);
+
+
+
+const eventRecordingSchema = new Schema({
+  date: String,
+  startTime: String,
+  endTime: String,
+  tally: Number,
+})
+
+// duration recording schema
+const durationRecordingSchema = new Schema({
+  date: String,
+  startTime: String,
+  endTime: String
+})
+
+//latency recording schema
+const latencyRecordingSchema = new Schema({
+  date: String,
+  startTime: String,
+  studentStartTime: String
+})
+
+//interval recording schema
+const intervalRecordingSchema = new Schema({
+  date: String,
+  intervalLength: Number, //(minutes)
+  intervals: [{ interval: Number, eventOccurred: Boolean}]
+})
+
+const teacherSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  students: [{ type: Schema.Types.ObjectId, ref: "student" }]
+})
+
+const Teacher = mongoose.model("teacher", teacherSchema);
+
+const EventData = mongoose.model("eventData", eventRecordingSchema);
+const DurationData = mongoose.model("durationData", durationRecordingSchema);
+const LatencyData = mongoose.model("latencyData", latencyRecordingSchema);
+const IntervalData = mongoose.model("intervalData", intervalRecordingSchema);
+
+const studentSchema = new Schema({
+  firstName: String,
+  //Bob
+  lastName: String,
+  //Ross
+  dob: String,
+  // 06/12/2005
+  disabilities: [String],
+  behaviors: [behaviorSchema],
+})
+
+const Student = mongoose.model("student", studentSchema);
+
+// FAKE DATA
+
+
+
+let student1 = new Student({
+  firstName: "Spalding",
+  lastName: "Vance",
+  dob: "06/16/1997",
+})
+
+let behavior1 = new Behavior({
+  behaviorName: "behavior1",
+  recordingType: "eventRecording"
+})
+
+let teacher1 = new Teacher({
+  firstName: "Ann",
+  lastName: "Schnabel"
+})
+
+
+
+
+let newEventData = new EventData({
+  date: "08/12/20",
+  behavior: "behavior1",
+  startTime: "10:00AM",
+  endTime: "11:00AM",
+  tally: 10
+})
+
+
+let newEventData2 = new EventData({
+  date: "08/12/20",
+  behavior: "behavior1",
+  startTime: "11:00AM",
+  endTime: "12:00PM",
+  tally: 4
+})
+
+
+behavior1.data.push(newEventData2);
+student1.behaviors.push(behavior1);
+student1.save();
+
+teacher1.students.push(student1);
+
+teacher1.save();
--- server/server.js
+++ server/server.js
@@ -1,3 +1,45 @@
+// const recordingTypes = [{"Event": "Number"}, {"Whole-Interval": }, "Duration", "Latency", "Partial-Intervention", "Partial-Interval"]
+// event recording
+// type- Number
+// date and time
+// for input- date, time window, tally
+
+
+
+// a student have behaviors
+// behaviors have data and a recording type
+// data depends on recording type
+
+
+// Event recording data
+// {
+//   tally: Number,
+//   date: String,
+//   startTime: Number,
+//   endTime: Number
+// }
+
+
+//partial interval recording
+  // whgether the behavior happened at any time during the interval
+    // underestimates high-frequency behavior and overestimates duration
+
+// whole interval recording is used to increase a behavior
+// partial interval recording is used to decrease behavior
+
+
+//Whole-Interval recording
+  // record interval where data occurs (entire duration)
+  // consistent intervals
+  // if the behavior doesn't span the entire interval, mark it as NO, if the behavior did extend for the entire interval mark it as YES
+
+
+//partial-interval recording
+  // used when 
+    // difficult to tell when the behavior begins or ends
+    // behavior occurs at such a high rate that its difficult to count
+  //record if behavior occured during an interval
+
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
@@ -7,3 +49,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const keys = require('./config/keys');
 const moment = require("moment");
+
+const Schema = mongoose.Schema;
+
+
+
+
+
+
+//
+
+
+//student has behaviors
+// each behavior has data
