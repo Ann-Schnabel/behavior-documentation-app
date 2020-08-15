999e950031244d8aa47a50a0800836d058a0cf33
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-08-15 15:10:28 -0400

    started on student list responsiveness

--- server/client/src/components/StudentList.js
+++ server/client/src/components/StudentList.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import '../styles/StudentList.css';
+import { NavLink } from 'react-router-dom';
 
 
 function StudentList() {
   return (
     <div>
       <div id='student-container'>
-        <h2>Students</h2>
-        <h2>+</h2>
+        <div id='student-container-header'>
+          <h2>Students</h2>
+          <h2 className='add'>+</h2>
+        </div>
+        <NavLink to='/home/:id' className='student-links'>
+          Ann Schnabel
+        </NavLink>
+        <NavLink to='/home/:id' className='student-links'>
+          Spalding Vance
+        </NavLink>
+        <NavLink to='/home/:id' className='student-links'>
+          Will Schnabel
+        </NavLink>
+        <NavLink to='/home/:id' className='student-links'>
+          Really really long name
+        </NavLink>
       </div>
     </div>
   );
