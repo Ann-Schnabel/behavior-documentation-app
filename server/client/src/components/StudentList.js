af9cc925e66e0c68418ba5242629499a461ee53a
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-08-16 13:08:24 -0400

    DropDown portion works manually

--- server/client/src/components/StudentList.js
+++ server/client/src/components/StudentList.js
@@ -1,31 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/StudentList.css';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 
 
-function StudentList() {
-  return (
-    <div>
-      <div id='student-container'>
-        <div id='student-container-header'>
-          <h2>Students</h2>
-          <h2 className='add'>+</h2>
+class StudentList extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      dropDown: true
+    }
+  }
+
+  toggleStudents = () => {
+    this.setState({dropDown: !this.state.dropDown})
+  }
+
+  screenSizeDropDownDecision() {
+    if (window.innerWidth < 600) {
+      this.setState({dropDown: false})
+    } else this.setState({dropDown: true})
+  }
+
+  renderStudents() {
+    const students = ['Ann Schnabel', 'Spalding Vance', 'Pierre Lourens'];
+
+    return students.map(student => {
+      return (
+        <NavLink to='/home/:id' className='student-links'>
+            {student}
+        </NavLink>
+      )
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <div id='student-container'>
+          <div id='student-container-header'>
+            <h2>Students</h2>
+            {this.state.dropDown && <FontAwesomeIcon className='drop-down-icon' onClick={this.toggleStudents} icon={faChevronDown} size='3x'/> }
+            {!this.state.dropDown && <FontAwesomeIcon className='drop-down-icon' onClick={this.toggleStudents} icon={faChevronUp} size='3x'/> }
+          </div>
+          {this.state.dropDown && this.renderStudents()}
         </div>
-        <NavLink to='/home/:id' className='student-links'>
-          Ann Schnabel
-        </NavLink>
-        <NavLink to='/home/:id' className='student-links'>
-          Spalding Vance
-        </NavLink>
-        <NavLink to='/home/:id' className='student-links'>
-          Will Schnabel
-        </NavLink>
-        <NavLink to='/home/:id' className='student-links'>
-          Really really long name
-        </NavLink>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default StudentList;
+
+
+{/* <NavLink to='/home/:id' className='student-links'>
+  Ann Schnabel
+</NavLink>
+<NavLink to='/home/:id' className='student-links'>
+  Spalding Vance
+</NavLink>
+<NavLink to='/home/:id' className='student-links'>
+  Will Schnabel
+</NavLink>
+<NavLink to='/home/:id' className='student-links'>
+  Really really long name
+</NavLink>  */}
